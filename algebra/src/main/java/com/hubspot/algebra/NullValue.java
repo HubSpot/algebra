ca56c5d9f7f5b6e6dd965d3399306d9ab96cacd1
Steve Gutz <sgutz@hubspot.com>
2018-08-09 14:49:58 -0400

    Deprecate direct access of enum constant

--- algebra/src/main/java/com/hubspot/algebra/NullValue.java
+++ algebra/src/main/java/com/hubspot/algebra/NullValue.java
@@ -2,6 +2,10 @@ package com.hubspot.algebra;
 
 public enum NullValue {
   // We name it this instead of INSTANCE for more human-readable JSON
+  /**
+   * @deprecated Call {@link NullValue#get()} instead of accessing this enum constant directly.
+   */
+  @Deprecated
   NULL_VALUE;
 
   public static NullValue get() {
