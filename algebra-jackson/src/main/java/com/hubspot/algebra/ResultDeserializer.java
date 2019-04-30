c61e48a0d14f0543f75e13cb6a58a2891cdecebb
Jonathan Haber <jhaber@hubspot.com>
2019-04-30 09:09:46 -0400

    Unused imports

--- algebra-jackson/src/main/java/com/hubspot/algebra/ResultDeserializer.java
+++ algebra-jackson/src/main/java/com/hubspot/algebra/ResultDeserializer.java
@@ -7,13 +7,11 @@ import static com.hubspot.algebra.ResultModule.OK_FIELD_NAME;
 import java.io.IOException;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.ObjectCodec;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.hubspot.algebra.ResultModule.Case;
