fe068abc3d7f0d2e5243adfc73b6926af6b95d64
mmedina <mmedina@hubspot.com>
2023-01-24 15:12:57 -0500

    Move tests

--- algebra-testing/src/test/java/com/hubspot/assertj/algebra/api/ResultAssertTest.java
+++ algebra-testing/src/test/java/com/hubspot/assertj/algebra/api/ResultAssertTest.java
@@ -4,11 +4,7 @@ import com.hubspot.algebra.Result;
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static com.hubspot.assertj.algebra.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class ResultAssertTest {
@@ -46,26 +42,6 @@ public class ResultAssertTest {
   @Test
   public void itPassesWhenExtractingErrValueOnErr() throws Exception {
     assertThat(Result.err(1)).extractingErr().isEqualTo(1);
-  }
-
-  @Test
-  public void itPassesWhenConsumingOkOnIfOkOrElseErr() throws Exception {
-    List<String> okResults = new ArrayList<>();
-    List<String> errorResults = new ArrayList<>();
-    Result<String, String> okResult = Result.ok("test");
-    okResult.ifOkOrElseError(okResults::add, errorResults::add);
-    assertThat(okResults).contains(okResult.unwrapOrElseThrow());
-    assertThat(errorResults).isEmpty();
-  }
-
-  @Test
-  public void itPassesWhenConsumingErrorOnIfOkOrElseErr() throws Exception {
-    List<String> okResults = new ArrayList<>();
-    List<String> errorResults = new ArrayList<>();
-    Result<String, String> errorResult = Result.err("test");
-    errorResult.ifOkOrElseError(okResults::add, errorResults::add);
-    assertThat(okResults).isEmpty();
-    assertThat(errorResults).contains(errorResult.unwrapErrOrElseThrow());
   }
 
   @Test
