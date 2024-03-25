34a3707e5b8cff49895cdf55582f6f64d6763589
mmedina <mmedina@hubspot.com>
2024-03-25 15:57:18 -0400

    Rename variables

--- algebra/src/test/java/com/hubspot/algebra/ResultTest.java
+++ algebra/src/test/java/com/hubspot/algebra/ResultTest.java
@@ -203,23 +203,23 @@ public class ResultTest {
 
   @Test
   public void itConvertsListOfResultsToResultOfList() {
-    List<Result<Integer, String>> xs = ImmutableList.of(
+    List<Result<Integer, String>> results = ImmutableList.of(
       Result.ok(1),
       Result.ok(2),
       Result.ok(3)
     );
-    Result<List<Integer>, String> rxs = Result.all(xs);
-    assertThat(rxs.unwrapOrElseThrow()).containsExactly(1, 2, 3);
+    Result<List<Integer>, String> actual = Result.all(results);
+    assertThat(actual.unwrapOrElseThrow()).containsExactly(1, 2, 3);
   }
 
   @Test
   public void itConvertsListOfResultsToFirstError() {
-    List<Result<Integer, String>> xs = ImmutableList.of(
+    List<Result<Integer, String>> results = ImmutableList.of(
       Result.ok(1),
       Result.ok(2),
       Result.err("error")
     );
-    Result<List<Integer>, String> rxs = Result.all(xs);
-    assertThat(rxs.unwrapErrOrElseThrow()).isEqualTo("error");
+    Result<List<Integer>, String> actual = Result.all(results);
+    assertThat(actual.unwrapErrOrElseThrow()).isEqualTo("error");
   }
 }
